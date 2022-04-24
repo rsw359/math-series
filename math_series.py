f100f1a026364278ecbba556c590a5241d69c81d
Roger Wells <rsw359@gmail.com>
2022-04-24 20:07:28 +0900

    added doc strings

--- math_series.py
+++ math_series.py
@@ -1,4 +1,4 @@
-
+# returns the nth value in the fibonacci series
 def fibonacci(num):
     if num == 0:
         return 0
@@ -8,6 +8,7 @@ def fibonacci(num):
         return fibonacci(num - 2) + fibonacci(num - 1)
 
 
+# returns the nth value in the lucas numbers
 def lucas(num):
     if num == 0:
         return 2
@@ -17,10 +18,11 @@ def lucas(num):
         return lucas(num - 2) + lucas(num - 1)
 
 
-def sum_series(num, first_num = 0 , second_num = 1):
+# returns the fibonacci sequence, lucas sequence, or a completely new sequence based on the input
+def sum_series(num, first_num=0, second_num=1):
     if num == 0:
         return first_num
     elif num == 1:
         return second_num
     else:
-       return sum_series(num-1, first_num, second_num) + sum_series(num-2, first_num, second_num)
+        return sum_series(num - 1, first_num, second_num) + sum_series(num - 2, first_num, second_num)
